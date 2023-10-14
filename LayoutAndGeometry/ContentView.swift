0b1cfebe3bfcb8ab8f61868dea5f451df5bd32fa
Alex Bonder <lexbonder@gmail.com>
2023-10-14 12:36:53 -0400

    Hacking with Swift - 100 Days of SwiftUI - Day 94

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+//    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+//                            .background(colors[index % 7])
+                            .background(Color(hue: min(geo.frame(in: .global).midY / fullView.size.height, 1.0), saturation: 1, brightness: 1))
+                            .opacity(geo.frame(in: .global).midY / 200)
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .scaleEffect(max(0.5, geo.frame(in: .global).midY / fullView.frame(in: .global).midY))
+                            .onTapGesture {
+                                print(geo.frame(in: .global).midY / fullView.size.height)
+                            }
+                    }
+                    .frame(height: 40)
+                }
+            }
         }
-        .padding()
     }
 }
 
